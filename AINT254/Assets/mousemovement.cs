f6b0eea15016525baee52f375631ed57918ee644
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-10-04 15:29:28 +0100

    testing some initial pancake flipping movement

---
+++ AINT254/Assets/mousemovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mousemovement : MonoBehaviour {
+
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+
+    float forwardRotation = 0;
+    float sidewaysRotation = 0;
+
+    void Update()
+    {
+        if(forwardRotation < -20 || forwardRotation > 20)
+        {
+            forwardRotation = 20;           
+        }
+        else
+        {
+            forwardRotation = horizontalSpeed * Input.GetAxis("Mouse Y");
+        }
+
+        if (sidewaysRotation < -20 || sidewaysRotation > 20)
+        {
+            sidewaysRotation = 20;
+        }
+        else
+        {
+            sidewaysRotation = verticalSpeed * Input.GetAxis("Mouse X");
+        }
+
+        transform.Rotate(forwardRotation, 0, (-1 * sidewaysRotation));
+    }
+}
