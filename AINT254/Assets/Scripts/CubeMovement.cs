68367ac9dbb2d0021fd78bf8b60b5be3263869d5
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-10-05 15:57:24 +0100

    vectors summary

---
+++ AINT254/Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    private Transform cubeTransform;
+    public float speed = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        cubeTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cubeTransform.position += transform.forward * speed;
+    }
+}
