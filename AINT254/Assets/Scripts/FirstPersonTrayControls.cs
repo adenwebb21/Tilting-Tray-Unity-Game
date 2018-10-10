c9d462d5f44b8e46a376bb2c585657e3e7c5d507
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-10-10 18:35:16 +0100

    added a second scene with a first person prototype

---
+++ AINT254/Assets/Scripts/FirstPersonTrayControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonTrayControls : MonoBehaviour
+{
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+
+    float forwardRotation = 0;
+    float sidewaysRotation = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardRotation = verticalSpeed * Input.GetAxis("Mouse X");
+        sidewaysRotation = horizontalSpeed * Input.GetAxis("Mouse Y");
+
+
+        transform.Rotate(sidewaysRotation, 0, (-1 * forwardRotation));
+    }
+}
