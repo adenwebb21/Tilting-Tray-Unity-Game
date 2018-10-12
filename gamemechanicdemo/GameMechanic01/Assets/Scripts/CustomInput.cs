b66c3db139840424aad249c2d427611e08fa31db
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-10-12 13:43:06 +0100

    added scene for angry bird gameplay mechanic demo

---
+++ gamemechanicdemo/GameMechanic01/Assets/Scripts/CustomInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomInput : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(5.0f, 5.0f, 200.0f, 30.0f), "Horizontal axis: " + Input.GetAxis("Horizontal"));
+    }
+
+}
