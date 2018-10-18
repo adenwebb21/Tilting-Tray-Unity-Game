e8b85289f52b9a3733f31aa18f6d2daa1b5c2b74
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-10-18 14:27:47 +0100

    added basic for scorekeeper plsu remove objects when they fall ooff

---
+++ AINT254/Assets/Scripts/RemoveTrayObj.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveTrayObj : MonoBehaviour
+{
+    private Transform itemTransform;
+    public GameObject scoreManager;
+
+    void Start()
+    {
+        itemTransform = transform;
+    }
+
+    private void Update()
+    {
+        if(itemTransform.position.y <= 0.1)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ AINT254/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public int score;
+
+    private string scoreString = "Score: ";
+    private GUIStyle style = new GUIStyle();
+
+    private void OnGUI()
+    {
+        style.fontSize = 40;
+        style.normal.textColor = Color.white;
+        GUI.Label(new Rect(10, 10, 100, 100), scoreString, style);
+    }
+
+    void Start()
+    {
+        score = GameObject.FindGameObjectsWithTag("TrayItems").Length;
+        scoreString += score.ToString();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ReduceScore()
+    {
+        if(score > 0)
+        {
+            score--;
+        }
+    }
+}
