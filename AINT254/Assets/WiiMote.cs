98fd9a1bd28d0c0aeabeeee74a28bb6e261e639f
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-11-14 23:36:05 +0000

    tried (unsuccessfully) to set up wiimote test

---
+++ AINT254/Assets/WiiMote.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WiimoteApi;
+
+public class WiiMote : MonoBehaviour {
+
+    void InitWiimotes()
+    {
+        WiimoteManager.FindWiimotes(); // Poll native bluetooth drivers to find Wiimotes
+        foreach (Wiimote remote in WiimoteManager.Wiimotes)
+        {
+            remote.RumbleOn = true;
+        }
+    }
+    void FinishedWithWiimotes()
+    {
+        foreach (Wiimote remote in WiimoteManager.Wiimotes)
+        {
+            WiimoteManager.Cleanup(remote);
+        }
+    }
+
+    private void Start()
+    {
+        InitWiimotes();
+    }
+}
