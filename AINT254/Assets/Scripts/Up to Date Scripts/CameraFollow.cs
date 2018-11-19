70cd9dca2209cd2aae41d6e4b42e1b77929a65f5
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-11-19 10:57:11 +0000

    working on fixing up the camera follow issues from before - started using the smoothfollow script

--- AINT254/Assets/Scripts/Up to Date Scripts/CameraFollow.cs
+++ AINT254/Assets/Scripts/Up to Date Scripts/CameraFollow.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-    public Transform target;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float smoothSpeed = 1f;
+    [SerializeField]
+    private Vector3 offset;
 
-    public float smoothSpeed = 1f;
-
-    public Vector3 offset;
-
-    private Vector3 velocity = Vector3.zero;
-    private Vector3 targetRotation = new Vector3();
+    [SerializeField]
+    private float distance = 10.0f;
+    // the height we want the camera to be above the target
+    [SerializeField]
+    private float height = 5.0f;
 
     private void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
-        transform.position = smoothedPosition;
+        //float desiredRotation = target.eulerAngles.y;
+        //float desiredHeight = target.position.y + height;
 
-        targetRotation.y = target.rotation.y;
+        //float currentRotation = transform.eulerAngles.y;
+        //float currentHeight = transform.position.y;
 
-        transform.rotation = Quaternion.Euler(targetRotation);
+        //currentRotation = Mathf.LerpAngle(currentRotation, desiredRotation, smoothSpeed * Time.deltaTime);
+        //currentHeight = Mathf.Lerp(currentHeight, desiredHeight, smoothSpeed * Time.deltaTime);
+
+        //Quaternion currentRotationQuat = Quaternion.Euler(0, currentRotation, 0);
+
+        //transform.position = target.position;
+        //transform.position -= currentRotation * Vector3.forward * distance;
+
+        //transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+        //transform.LookAt(target);
+
+        transform.position = new Vector3(target.position.x, 2, target.position.z + (target.forward.z * -2));
+        transform.LookAt(target);
     }
 }
