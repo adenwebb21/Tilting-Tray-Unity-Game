85b8197c7d18b1e6db6e1319404c1bcb56d665cf
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-11-20 12:19:07 +0000

    roation never goes above 90?????????

--- AINT254/Assets/Scripts/Up to Date Scripts/CameraFollow.cs
+++ AINT254/Assets/Scripts/Up to Date Scripts/CameraFollow.cs
@@ -6,38 +6,21 @@ public class CameraFollow : MonoBehaviour {
 
     [SerializeField]
     private Transform target;
-    [SerializeField]
-    private float smoothSpeed = 1f;
+
     [SerializeField]
     private Vector3 offset;
 
-    [SerializeField]
-    private float distance = 10.0f;
-    // the height we want the camera to be above the target
-    [SerializeField]
-    private float height = 5.0f;
+    public float targetQuat;
 
     private void LateUpdate()
     {
-        //float desiredRotation = target.eulerAngles.y;
-        //float desiredHeight = target.position.y + height;
+        transform.rotation = new Quaternion(0, target.rotation.y, 0, 1);
+        targetQuat = target.rotation.y;
+        transform.position = target.position;
+        transform.position -= transform.rotation * Vector3.forward * offset.z;
 
-        //float currentRotation = transform.eulerAngles.y;
-        //float currentHeight = transform.position.y;
+        transform.position = new Vector3(transform.position.x, offset.y, transform.position.z);
 
-        //currentRotation = Mathf.LerpAngle(currentRotation, desiredRotation, smoothSpeed * Time.deltaTime);
-        //currentHeight = Mathf.Lerp(currentHeight, desiredHeight, smoothSpeed * Time.deltaTime);
-
-        //Quaternion currentRotationQuat = Quaternion.Euler(0, currentRotation, 0);
-
-        //transform.position = target.position;
-        //transform.position -= currentRotation * Vector3.forward * distance;
-
-        //transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
-
-        //transform.LookAt(target);
-
-        transform.position = new Vector3(target.position.x, 2, target.position.z + (target.forward.z * -2));
         transform.LookAt(target);
     }
 }
