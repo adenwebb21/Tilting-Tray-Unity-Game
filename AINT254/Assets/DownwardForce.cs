1e3d40a0ba45ef5944fd2017521b0adfc40d1ce9
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-11-25 13:57:40 +0000

    started work on a labyrinth prototype

---
+++ AINT254/Assets/DownwardForce.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownwardForce : MonoBehaviour {
+
+	void FixedUpdate()
+    {
+        gameObject.GetComponent<Rigidbody>().AddForce(0, -100, 0);
+    }
+}
---
+++ AINT254/Assets/Scripts/Up to Date Scripts/LabyrinthMover.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrinthMover : MonoBehaviour
+{
+
+    [SerializeField]
+    private float horizontalSpeed = 2.0F;
+    [SerializeField]
+    private float verticalSpeed = 2.0F;
+
+    private float forwardRotation = 0;
+    private float sidewaysRotation = 0;
+
+    private float currentForwardRotation;
+    private float currentSidewaysRotation;
+
+    private Rigidbody trayRigidbody;
+    private Quaternion trayRotation;
+    private Transform labTransform;
+
+    private bool reachedLimit = false;
+    public float t;
+    private Vector3 eulers;
+
+    private Vector3 forwardRotationVector, sidewaysRotationVector;
+
+    private void Start()
+    {
+        trayRigidbody = gameObject.GetComponent<Rigidbody>();
+        labTransform = transform;
+        reachedLimit = false;
+    }
+
+    void Update()
+    {
+        sidewaysRotation = horizontalSpeed * Input.GetAxis("Mouse X");
+        forwardRotation = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        //forwardRotationVector = transform.right * forwardRotation;
+        //sidewaysRotationVector = transform.forward * sidewaysRotation;
+
+        forwardRotationVector = new Vector3(forwardRotation, 0, 0);
+        sidewaysRotationVector = new Vector3(0, 0, sidewaysRotation);
+
+
+        eulers = transform.rotation.eulerAngles;
+        t = eulers.x;
+
+        if ((eulers.x > 350 || eulers.x < 10) && (eulers.z > 355 || eulers.z < 5))
+        {
+            reachedLimit = false;
+        }
+        else
+        {
+            reachedLimit = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //if (reachedLimit)
+        //{
+        //    trayRigidbody.angularDrag = Mathf.Max(Mathf.Abs(eulers.x), Mathf.Abs(eulers.z)) * 0.5f;            
+        //}
+
+        transform.rotation = Quaternion.Euler(eulers.x, 0, eulers.z);
+        trayRigidbody.angularVelocity = new Vector3(forwardRotation, 0, -sidewaysRotation);
+    }
+}
